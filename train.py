d77317c7f948f9bbc745fda113c6c6dff0d6d5c2
bhavanagopakumar <bhavanag19@gmail.com>
2024-12-06 19:07:46 +0530

    updated batch size

--- train.py
+++ train.py
@@ -25,7 +25,7 @@ def get_transforms(is_train=True):
             transforms.Normalize((0.1307,), (0.3081,))
         ])
 
-def train(epochs=1, batch_size=16, learning_rate=0.01):
+def train(epochs=1, batch_size=64, learning_rate=0.01):
     # Set device
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     
